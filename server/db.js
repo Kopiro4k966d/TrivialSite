ddf3778e5a1ca06863a48d4f04c3e2dee113f2df
Kopiro4k966d <132064646+Kopiro4k966d@users.noreply.github.com>
2026-08-03 05:32:22 +0800

    Update db.js

--- server/db.js
+++ server/db.js
@@ -1,5 +1,22 @@
-import pg from 'pg';
+import pg from "pg";
+
 const { Pool } = pg;
-const connectionString = process.env.DATABASE_URL;
-const pool = new Pool({ connectionString, ssl: connectionString && !connectionString.includes('localhost') ? { rejectUnauthorized: false } : false });
+
+const connectionString =
+  process.env.POSTGRES_PRISMA_URL ||
+  process.env.POSTGRES_URL ||
+  process.env.DATABASE_URL;
+
+if (!connectionString) {
+  throw new Error("Database connection string is missing");
+}
+
+const pool = new Pool({
+  connectionString,
+  ssl: {
+    rejectUnauthorized: false
+  },
+  max: 5
+});
+
 export default pool;
